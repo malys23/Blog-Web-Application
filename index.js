07ec713d474376b776d2aaad9aa16e9ccb543d48
Alyona M <alyona.maliassova@gmail.com>
2026-01-28 17:29:35 -0600

    Back end function
    Implementing al backend function -
    1. creating posts
    2. Deleting posts
    3. Editing posts
    4. Implementing basic structure and menus

--- index.js
+++ index.js
@@ -4,29 +4,92 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+//Middleware
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Container for posts
+let posts = [];
+
+//Post constructor
+function Post(title, text){
+    this.title = title;
+    this.text = text;
+    this.time = new Date().toLocaleString();
+}
+
+//Add post
+function addPost(title, text){
+    let post = new Post(title, text);
+    posts.push(post);
+}
+
+//Remove post
+function removePost(index){
+    posts.splice(index, 1);
+}
+
+//Edit post
+function editPost(index, title, text){
+    posts[index] = new Post(title, text);
+}
+
+/**
+ * Paths
+ */
+// Home
 app.get("/", (req, res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: posts});
 });
 
+//About page
 app.get("/about", (req, res)=>{
     res.render("about.ejs");
 });
 
-app.get("/post", (req, res)=>{
-    res.render("post.ejs");
+//Post Creation
+app.get("/thePost", (req, res)=>{
+    res.render("thePost.ejs");
 });
 
+//Post Save
 app.post("/submit", (req, res) => {
-    var rawTime = new Date();
-    const time = rawTime.toLocaleString();
-    const title = req.body.title
-    const text = req.body.text
-    res.render("index.ejs", {time: time, title: title, text: text});
-})
+    let title = req.body.title;
+    let text = req.body.text;
+
+    addPost(title, text);
+    res.redirect("/");
+});
+
+//Post View
+app.get("/view/:id", (req, res) => {
+    let index = req.params.id;
+    let post = posts[index];
+    res.render("view.ejs", {postID: index, title: post.title, text: post.text})
+});
+
+//Post Edit
+app.get("/edit/:id", (req, res) =>{
+    let index = req.params.id;
+    let post = posts[index];
+    res.render("thePost.ejs", {postID: index, title: post.title, text: post.text});
+});
+
+//Submit Edit
+app.post("/update", (req, res) => {
+    let index = req.body.index;
+    let title = req.body.title;
+    let text = req.body.text;
+    editPost(index, title, text);
+    res.redirect("/");
+});
+
+//Post Delete
+app.post("/delete", (req, res) => {
+    let index = req.body.postID;
+    removePost(index);
+    res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
